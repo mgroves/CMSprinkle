a2e65e2f77a0c20698f2a37009f48c96e02cfc01
Matthew D. Groves <me@mgroves.com>
2023-11-23 12:36:21 -0500

    fix for content is null

--- CMSprinkle/StringExtensions.cs
+++ CMSprinkle/StringExtensions.cs
@@ -8,7 +8,7 @@ public static class StringExtensions
     public static string ToTruncateMarkdown(this string markdown, int maxLength)
     {
         // Convert Markdown to plain text
-        string plainText = Markdown.ToPlainText(markdown);
+        string plainText = Markdown.ToPlainText(markdown ?? "");
 
         // Truncate the plain text
         return plainText.Length <= maxLength ? plainText : plainText.Substring(0, maxLength) + "...";
